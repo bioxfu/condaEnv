aa0191dd14cd817144d57c8c9fa26d1de01915a8
Fu Xing <bio.xfu@gmail.com>
2018-07-23 14:23:39 +0800

    update

--- remove_pkgs.sh
+++ remove_pkgs.sh
@@ -1,3 +1,5 @@
+source activate gmatic
+
 conda list bioconductor|grep -v '#'|sed -r 's/ .+//'|xargs -I {} conda remove -y {}
 
 conda list r-|grep '^r-'|sed -r 's/ .+//'|xargs -I {} conda remove -y {}
