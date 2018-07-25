26df13d3c1660a42dcaba6802891f493dc55680f
Fu Xing <bio.xfu@gmail.com>
2018-07-25 11:06:20 +0800

    set channels

--- install_conda.sh
+++ install_conda.sh
@@ -1,2 +1,8 @@
 wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
 bash Miniconda2-latest-Linux-x86_64.sh
+source ~/.bashrc
+
+conda config --add channels defaults
+conda config --add channels conda-forge
+conda config --add channels bioconda
+
