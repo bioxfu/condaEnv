6d4b3ee21ae6b29cf786d5ab2f10d26f56dc5029
Fu Xing <bio.xfu@qq.com>
2020-01-10 10:17:46 +0800

    update to miniconda3

--- install_conda.sh
+++ install_conda.sh
@@ -1,8 +1,10 @@
-wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
-bash Miniconda2-latest-Linux-x86_64.sh
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
 source ~/.bashrc
 
 conda config --add channels defaults
 conda config --add channels conda-forge
 conda config --add channels bioconda
 #conda config --add channels anaconda
+
+conda config --set auto_activate_base false
